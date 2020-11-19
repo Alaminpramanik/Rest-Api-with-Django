1dbec195db8f9d6d499ac8cb423d885771b3f93b
unknown <alamin493641@gmail.com>
2020-11-19 19:00:18 +0600

    master

--- grayspaceit/settings.py
+++ grayspaceit/settings.py
@@ -135,6 +135,7 @@ STATIC_ROOT = str(BASE_DIR / "staticfiles")
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [str(BASE_DIR / "static")]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
